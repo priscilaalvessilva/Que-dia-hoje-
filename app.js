f5261e4192c88f76d560ec874ea8134b1ca6a0d4
PRISCILA ALVES <75861909+priscilaalvessilva@users.noreply.github.com>
2025-01-22 18:32:39 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+alert('Seja Bem-vindo ao meu site!');
+let dia = prompt('Qual é o dia da semana de hoje?');
+
+if (dia === 'segunda') {
+    alert('Boa semana!'); }
+
+    else if (dia === 'terça') {
+        alert('Boa semana!'); }
+
+        if (dia === 'quarta') {
+            alert('Boa semana!'); }
+            
+            else if (dia === 'quinta') {
+                alert('Boa semana!'); }
+
+                if (dia === 'sexta') {
+                    alert('Boa semana!'); }
+                    
+                    else if (dia === 'sabado') {
+                        alert('Bom final de semana!'); }
+
+                        if (dia === 'domingo') {
+                            alert('Bom final de semana!'); }
+                            
+                        
